b4b5f1cc6574b6b8d9491ecd40c7faadb8d34ea6
valsoa <manoaniainavalisoa@gmail.com>
2025-01-24 08:42:27 +0100

    push login user

--- src/mg/itu/model/User.java
+++ src/mg/itu/model/User.java
@@ -1,70 +1,94 @@
-package mg.itu.model;
+package utils;
 
-import bean.ClassMAPTable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class User extends ClassMAPTable {
+public class User {
+    private int id;
+    private int idCommune;
+    private String username;
+    private String password;
+    private Connection connection;
 
-    String id;
-    String idCommune;
-    String userName;
-    String userPassword;
-
-    public User() {
-        super.setNomTable("users");
+    // Constructeur par défaut
+    public User(Connection connection) {
+        this.connection = connection;
     }
 
-    public User(String id, String idCommune, String userName, String userPassword) {
-        this.id = id;
-        this.idCommune = idCommune;
-        this.userName = userName;
-        this.userPassword = userPassword;
-    }
-
-    @Override
-    public String[] getMotCles() {
-        String[] motCles = { "id", "id_commune", "user_name", "user_password" };
-        return motCles;
-    }
-
-    @Override
-    public String getAttributIDName() {
-        return "id";
-    }
-
-    @Override
-    public String getTuppleID() {
+    // Getters et Setters
+    public int getId() {
         return id;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
-    public String getIdCommune() {
+    public int getIdCommune() {
         return idCommune;
     }
 
-    public void setIdCommune(String idCommune) {
+    public void setIdCommune(int idCommune) {
         this.idCommune = idCommune;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getUserPassword() {
-        return userPassword;
+    public String getPassword() {
+        return password;
     }
 
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
+    public void setPassword(String password) {
+        this.password = password;
     }
+
+    // Méthode pour authentifier un utilisateur
+    public User login(String username, String password) throws SQLException {
+        String query = "SELECT id, id_commune, user_name, user_password " +
+                       "FROM users " +
+                       "WHERE user_name = ? AND user_password = ?";
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = connection.prepareStatement(query);
+            stmt.setString(1, username);
+            stmt.setString(2, password); // Assurez-vous que le mot de passe est bien haché avant la vérification
+
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                User user = new User(connection);
+                user.setId(rs.getInt("id"));
+                user.setIdCommune(rs.getInt("id_commune"));
+                user.setUsername(rs.getString("user_name"));
+                user.setPassword(rs.getString("user_password"));
+
+                return user;
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+
+        return null; // Retourne null si l'utilisateur n'est pas trouvé
+    }
+
+    
+
+    
+
 }
